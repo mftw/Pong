f39e9106961e7cfcfe514411552e8802d0c5dddc
TobiasLrsen <tobiash.larsen@outlook.dk>
2019-01-11 13:28:59 +0100

    Friday done

--- pointlogic.js
+++ pointlogic.js
@@ -1,2 +1,29 @@
-var leftPlayerScore = 0;
-var rightPlayerScore = 1;
+var playerOneScore = 0;
+var playerTwoScore = 0;
+
+function countPlayerScore(counter) {
+    return counter + 1;
+}
+
+function reset(score1, score2) {
+    score1 = 0;
+    score2 = 0;
+    cb();
+}
+
+function cb() {
+    document.getElementById("h1").textContent = "RESET";
+}
+
+
+
+
+document.getElementById("reset").addEventListener("click", function() {
+    reset(playerOneScore, playerTwoScore);  u
+});
+
+document.getElementById("player1").addEventListener("click", function() {
+    playerOneScore = countPlayerScore(playerOneScore);
+
+    console.log(playerOneScore);
+});
