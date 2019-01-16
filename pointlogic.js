cd2eb5e6e8b99cf75b73f1c8204c4d74a828deed
TobiasLrsen <tobiash.larsen@outlook.dk>
2019-01-16 14:54:39 +0100

    Pointsystem code updated

--- pointlogic.js
+++ pointlogic.js
@@ -2,37 +2,68 @@ var playerOneScore = 0;
 var playerTwoScore = 0;
 var leftScore = document.getElementById("leftScore");
 var rightScore = document.getElementById("rightScore");
+var p1 = document.getElementById("player1");
+var p2 = document.getElementById("player2");
 
 
-
-function countPlayerScore(counter) {
+function countPlayerScore(counter, win) {
+    if(counter == 10) {
+        console.log("winner: " + win);
+        winSituation();
+    }
     return counter + 1;
 }
 
-function reset() {
-    playerOneScore = 0;
-    playerTwoScore = 0;
+function scoreBoard() {
+    leftScore.innerHTML = "<h3>" + playerOneScore + "</h3>";
+    rightScore.innerHTML = "<h3>" + playerTwoScore + "</h3>";
 }
 
-leftScore.innerHTML = "<h3>" + playerOneScore + "</h3>";
-rightScore.innerHTML = "<h3>" + playerTwoScore + "</h3>";
+function resetScoreboard() {
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    scoreBoard();
+}
+
+function winSituation() {
+    console.log("Whatever der nu sker når en vinder");
+}
 
 
 
 
+
+// p1.addEventListener("click", function() {
+//     if (counter == 11) {
+//         console.log("winner");
+//         return;
+//     }
+//     counter = countPlayerScore(counter);
+//     console.log(counter);Player1Goal
+//     playerOneScore = counter 
+//     leftScore.innerHTML = "<p>" + playerOneScore + "</p>";
+//     console.log(leftScore.innerHTML);
+
+
+// });
+
+p1.addEventListener("click", function() {
+    playerOneScore.innerHTML = "<h3>" + playerOneScore + "</h3>";
+    if(playerOneScore == 11) {
+        return;
+    }
+    playerOneScore = countPlayerScore(playerOneScore, "player1");
+    scoreBoard();
+});
+
+p2.addEventListener("click", function() {
+    if(playerTwoScore == 11) {
+        return;
+    }
+    playerTwoScore = countPlayerScore(playerTwoScore, "player2");
+    scoreBoard();
+});
 
 document.getElementById("reset").addEventListener("click", function() {
-    reset();
-});
-
-document.getElementById("player1").addEventListener("click", function() {
-    playerOneScore = countPlayerScore(playerOneScore);
-
-    console.log(playerOneScore);
-});
-
-document.getElementById("player2").addEventListener("click", function() {
-    playerTwoScore = countPlayerScore(playerTwoScore);
-
-    console.log(playerTwoScore);
+    resetScoreboard();
 });
